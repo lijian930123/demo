3c63d549cd0d92e2c9e0863604e92dd1906a6e37
lijian930123 <598555351@qq.com>
2018-05-03 17:18:31 +0800

    加入日志记录

--- dubbo-provider/src/main/java/cn/firetech/DemoServiceImpl.java
+++ dubbo-provider/src/main/java/cn/firetech/DemoServiceImpl.java
@@ -1,11 +1,16 @@
 package cn.firetech;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service("demoService")
 public class DemoServiceImpl implements DemoService{
 
+    static final Logger logger = LoggerFactory.getLogger(DemoServiceImpl.class);
+
     @Override
     public String sayHello(String name) {
-        return name + "change";
+        logger.error("provider服务调用");
+        return name + " dubbo test, success!";
     }
 }
